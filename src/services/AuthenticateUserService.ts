a5d755e9bf5ba2601b5ef23688b30869b497d6e0
pedro guimaraes <pedrof.guima197@gmail.com>
2021-02-02 21:40:16 -0300

    finalização das aulas do modulo de node

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 import User from '../models/User';
 
@@ -22,13 +23,13 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ email });
 
     if (!user) {
-      throw new Error('incorrect email/password combination');
+      throw new AppError('incorrect email/password combination', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('incorrect email/password combination');
+      throw new AppError('incorrect passwordemail/password combination', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
