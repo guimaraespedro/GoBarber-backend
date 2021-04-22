a6c8906aeb14a3a7d474caae727b8364b299640b
pedro guimaraes <pedrof.guima197@gmail.com>
2021-04-21 23:33:08 -0300

    organizando o projeto

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,14 +1,16 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+
 import routes from './routes';
-import './database';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import '@shared/infra/typeorm';
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
